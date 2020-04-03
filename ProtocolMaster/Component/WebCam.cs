871e2dfba20da8967ecc4cb097cb10c86a6f0f87
joeltkurian <joeltkurian@gmail.com>
2020-04-03 13:24:42 -0500

    Create WebCam.cs

---
+++ ProtocolMaster/Component/WebCam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AForge.Video;
+
+namespace ProtocolMaster.Component
+{
+    class WebCam
+    {
+
+    }
+}
