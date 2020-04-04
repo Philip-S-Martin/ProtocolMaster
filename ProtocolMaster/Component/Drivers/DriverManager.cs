a0750bb2ee347aa8367af0289c2f89578f8623c1
Philip Martin <philipscottmartin01@gmail.com>
2020-04-04 18:49:26 -0500

    Revert "Revert "Revert "Revert "For Matt to View""""
    This reverts commit fc3d84dde649990d0f57432ee356e6042011f225.

---
+++ ProtocolMaster/Component/Drivers/DriverManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProtocolMaster.Component.Drivers
+{
+    class DriverManager
+    {
+        public static ConcurrentBag<string> currentPorts = new ConcurrentBag<string>();
+        private static ConcurrentDictionary<string, bool> allPorts = new ConcurrentDictionary<string, bool>();
+        public static bool TryAdd<T>(string targetPort) where T : Driver, new()
+        {
+            
+            Driver.Start<T>(targetPort);
+            return true;
+        }
+
+        public static bool TryRemove(string targetPort)
+        {
+
+        }
+
+        public static string[] AvailablePorts()
+        {
+            List<string> available = new List<string>();
+            foreach(KeyValuePair<string, bool> port in allPorts)
+            {
+                // try to connect to each and identify it
+            }
+        }
+    }
+}
