7a32eab52ac7501b1d75581e65f1e67c2cacfddf
joeltkurian <joeltkurian@gmail.com>
2020-04-05 12:37:55 -0500

    Joel T Code
    Adds the WebCam (non functional yet)

---
+++ ProtocolMaster/Component/WebCam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using Accord.Video.FFMPEG;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using Microsoft.Win32;
+using ProtocolMaster.Component;
+
+namespace ProtocolMaster.Component
+{
+    public class WebCam
+    {
+         
+    }
+}
