371340705ca2a029a0b8934c77f5cece0376175d
Philip-S-Martin <philipscottmartin01@gmail.com>
2021-02-17 18:32:29 -0600

    Published Protocol Data Validation
    Added data validation and automatic closing to the published protocol form. Not an elegant solution at the moment, but it works!

---
+++ ProtocolMasterWPF/Helpers/IsEnabledConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ProtocolMasterWPF.Helpers
+{
+    public class IsEnabledConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values.LongLength > 0)
+            {
+                foreach (var value in values)
+                {
+                    if (value is bool && (bool)value)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ProtocolMasterWPF/View/PublishedSelectView.xaml.cs
+++ ProtocolMasterWPF/View/PublishedSelectView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using ProtocolMasterWPF.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,6 +20,32 @@ namespace ProtocolMasterWPF.View
     /// </summary>
     public partial class PublishedSelectView : UserControl, ISelectView
     {
+        public static readonly DependencyProperty NewNameProperty =
+            DependencyProperty.Register("NewName", typeof(string), typeof(PublishedSelectView));
+        public string NewName
+        {
+            get { return (string)GetValue(NewNameProperty); }
+            set { SetValue(NewNameProperty, value); }
+        }
+        public static readonly DependencyProperty NewURLProperty =
+            DependencyProperty.Register("NewURL", typeof(string), typeof(PublishedSelectView));
+        public string NewURL
+        {
+            get { return (string)GetValue(NewURLProperty); }
+            set { SetValue(NewURLProperty, value); }
+        }
+
+        private int _numberOfValidationErrors = 0;
+        public bool HasNoValidationErrors => _numberOfValidationErrors == 0;
+
+        private void HandleValidationError(object sender, ValidationErrorEventArgs e)
+        {
+            if (e.Action == ValidationErrorEventAction.Added)
+                _numberOfValidationErrors++;
+            else
+                _numberOfValidationErrors--;
+            AddButton.IsEnabled = HasNoValidationErrors;
+        }
         public PublishedSelectView()
         {
             InitializeComponent();
@@ -26,7 +53,14 @@ namespace ProtocolMasterWPF.View
 
         public ListBox SelectList => SelectListBox;
 
+
         private void DeleteButton_Click(object sender, RoutedEventArgs e) => PublishedFileStore.Instance.Remove(SelectListBox.SelectedValue as PublishedFileStreamer);
-        private void AddButton_Click(object sender, RoutedEventArgs e) => PublishedFileStore.Instance.Add(NewNameText.Text, NewURLText.Text);
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            PublishedFileStore.Instance.Add(NewName, NewURL);
+            NewName = "";
+            NewURL = "";
+            AddPopup.IsPopupOpen = false;
+        }
     }
 }
